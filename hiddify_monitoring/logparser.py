5f49588b540fc14a436fd97e3f9bbff37e966c75
Hiddify <114227601+hiddify@users.noreply.github.com>
2022-12-11 11:38:14 +0000

    add new protocols

--- hiddify_monitoring/logparser.py
+++ hiddify_monitoring/logparser.py
@@ -3,7 +3,10 @@ from datetime import datetime
 from . import ipresolver
 import hashlib
 
-upstream_map = {'127.0.0.1:440':'monitoring','127.0.0.1:447': 'ss-faketls', '127.0.0.1:449': 'telegram-faketls', '127.0.0.1:448': 'v2ray', '127.0.0.1:445': 'vmess', '8.8.8.8:443': 'dns'}
+upstream_map = {'127.0.0.1:440': 'monitoring', '127.0.0.1:447': 'ss-faketls', '127.0.0.1:449': 'telegram-faketls', 
+                '127.0.0.1:448': 'v2ray', '127.0.0.1:445': 'vmess', '8.8.8.8:443': 'dns',
+                '127.0.0.1:1002': 'ss-faketls', '127.0.0.1:1001': 'telegram-faketls',
+                '127.0.0.1:1003': 'v2ray', '127.0.0.1:1004': 'vmess', '127.0.0.1:1005': 'trojan'}
 lineformat = re.compile(
     r"""\[(?P<dateandtime>\d{2}\/[a-z]{3}\/\d{4}:\d{2}:\d{2}:\d{2} (\+|\-)\d{4})\] (?P<ipaddress>\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}) (?P<upstream>[0-9.:\-\W]+) (?P<status>\d+) (?P<download>\d+) (?P<upload>\d+) (?P<connectiontime>\d+)""", re.IGNORECASE)
 
@@ -12,18 +15,22 @@ def parse(line):
     data = re.search(lineformat, line)
     if data:
         datadic = data.groupdict()
-        datadic['dateandtime'] = datetime.strptime(datadic['dateandtime'], "%d/%b/%Y:%H:%M:%S %z")
+        datadic['dateandtime'] = datetime.strptime(
+            datadic['dateandtime'], "%d/%b/%Y:%H:%M:%S %z")
         date = datadic['dateandtime'].strftime('%Y%m%d')
-        datadic['upstream'] = upstream_map.get(datadic['upstream'], datadic['upstream'])
+        datadic['upstream'] = upstream_map.get(
+            datadic['upstream'], datadic['upstream'])
         datadic['ipinfo'] = ipresolver.get(datadic['ipaddress'])
         datadic['connectiontime'] = float(datadic['connectiontime'])
         datadic['download'] = int(datadic['download'])
         datadic['upload'] = int(datadic['upload'])
         datadic['status'] = int(datadic['status'])
 
-        datadic['haship'] = haship(f"{datadic['ipinfo']['asn_name']}{date}{haship(datadic['ipaddress'])}")
+        datadic['haship'] = haship(
+            f"{datadic['ipinfo']['asn_name']}{date}{haship(datadic['ipaddress'])}")
         del datadic['ipaddress']
         return datadic
 
+
 def haship(str):
-    return int(hashlib.sha1(str.encode("utf-8")).hexdigest(), 16)% (10 ** 8)
+    return int(hashlib.sha1(str.encode("utf-8")).hexdigest(), 16) % (10 ** 8)
